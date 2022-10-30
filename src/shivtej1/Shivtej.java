3a3aeae365e1a14df45e5dec94083bb015bdcc26
HP <HP@LAPTOP-J77QTRLH>
2022-10-31 00:08:18 +0530

    First commit_1

---
+++ src/shivtej1/Shivtej.java
@@ -0,0 +1,11 @@
+package shivtej1;
+
+public class Shivtej 
+{
+     public static void main(String[] args)
+     {
+	System.out.println("good morning");
+	
+	
+     }
+}
---
+++ src/shivtej1/shinde.java
@@ -0,0 +1,12 @@
+package shivtej1;
+
+public class shinde {
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("shivtej shinde");
+		
+	}
+	
+}
